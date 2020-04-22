94eaab62e668dfc6b04f320cce9cc9eee2c5190e
Guilherme Prado <guilherme.oliveira@objective.com.br>
2020-04-22 18:45:55 -0300

    ⚡🎉 list command

--- src/commands/main.js
+++ src/commands/main.js
@@ -2,8 +2,10 @@ const helpCommand = require('./help')
 const configureCommand = require('./configure')
 const quotaCommand = require('./quota')
 const compressCommand = require('./compress')
+const listCommand = require('./list')
 
 module.exports = {
+    list: listCommand,
     help: helpCommand,
     configure: configureCommand.configure,
     quota: quotaCommand,
